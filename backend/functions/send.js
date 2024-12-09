efe251e4c3ea3acba75510171ef8d4a4af09963f
Gabriel <gabrielbortle@gmail.com>
2024-12-09 13:39:29 -0700

    changed local testing

--- backend/functions/send.js
+++ backend/functions/send.js
@@ -13,7 +13,7 @@ const transporter = nodemailer.createTransport({
 
 exports.handler = async (event) => {
   const headers = {
-    "Access-Control-Allow-Origin": "http://localhost:5173", // Replace with your Vite dev server URL
+    "Access-Control-Allow-Origin": "*", // Allow all origins for testing
     "Access-Control-Allow-Methods": "OPTIONS, POST",
     "Access-Control-Allow-Headers": "Content-Type",
   };
